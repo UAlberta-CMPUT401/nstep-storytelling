253f28af0db5ae10194aacb6db4e16cfbb7a1cb6
Théo <68571255+TheosTechGH@users.noreply.github.com>
2022-10-05 22:45:28 -0600

    Fixed lint and new component
    Fixed lint:
    - 'React' not found
    - 'expected LF but CRLF'
    
    New component:
    - Form.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Jashwanth!</h1>
+      <Form />
     </div>
   );
 }
---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,5 @@
+function Form() {
+  return <h3>Here is a form</h3>;
+}
+
+export default Form;
