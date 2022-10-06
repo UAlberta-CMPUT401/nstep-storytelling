65b669807a3cce2b81109d9891c55933b138f401
Théo <68571255+TheosTechGH@users.noreply.github.com>
2022-10-06 00:04:09 -0600

    Text input test on components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import Form from './components/Form';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('sport');
+  const showForms = true;
   return (
     <div className="App">
-      <Form />
+      {showForms
+        ? (
+          <>
+          <input type='text' onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+          }}/>
+            <Form name="eating activity" role="healthiness" />
+            <Form name="running activity" role={role}/>
+            <Form name="jumping activity  " />
+          </>
+        )
+        : <p>You cannot see the forms.</p>}
     </div>
   );
 }
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,5 +1,13 @@
-function Form() {
-  return <h3>Here is a form</h3>;
+function Form(props) {
+    return (
+        <>
+            <h3>
+                Form: {
+                props.name
+            } </h3>
+            {props.role ? <p>{props.role}</p> : <p>No role</p>}
+        </>
+    );
 }
 
 export default Form;
