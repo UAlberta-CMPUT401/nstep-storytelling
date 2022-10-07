22c2d7c0d90c3bc1c9b7ec790ffa73fbd89075ce
Théo <68571255+TheosTechGH@users.noreply.github.com>
2022-10-07 00:01:49 -0600

    Arrays for 'Form' component, eslint updates
    Arrays for 'Form' component:
    - Forms can now be stored in arrays instead of hardcoded
    
    eslint updates:
    - Now ignoring some unnecessarily stringent rules.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
-import { useState } from 'react';
 
 function App() {
   const [role, setRole] = useState('sport');
+  const [forms, setForms] = useState(
+    [
+      { name: 'samplename', role: 'samplerole' },
+      { name: 'samplename', role: 'samplerole' },
+      { name: 'samplename', role: 'samplerole' },
+      { name: 'samplename', role: 'samplerole' },
+      { name: 'samplename', role: 'samplerole' },
+    ],
+  );
   const showForms = true;
   return (
     <div className="App">
       {showForms
         ? (
           <>
-          <input type='text' onChange={(e) => {
-            console.log(e.target.value);
-            setRole(e.target.value);
-          }}/>
-            <Form name="eating activity" role="healthiness" />
-            <Form name="running activity" role={role}/>
-            <Form name="jumping activity  " />
+            <input
+              type="text"
+              onChange={(e) => {
+                console.log(e.target.value);
+                setRole(e.target.value);
+              }}
+            />
+            <div>
+              {forms.map((form) => {
+                return (<Form key={form.id} name={form.name} role={form.role} />);
+              })}
+            </div>
           </>
         )
         : <p>You cannot see the forms.</p>}
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,13 +1,18 @@
 function Form(props) {
-    return (
-        <>
-            <h3>
-                Form: {
+  return (
+    <>
+      <h3>
+        Form:
+        {
                 props.name
-            } </h3>
-            {props.role ? <p>{props.role}</p> : <p>No role</p>}
-        </>
-    );
+            }
+      </h3>
+      {props.role ? <p>{props.role}</p> : <p>No role</p>}
+      <button>
+        Update
+      </button>
+    </>
+  );
 }
 
 export default Form;
