5aa9ba06bdab101efdfe024ccb95b791f2f970a2
Théo <68571255+TheosTechGH@users.noreply.github.com>
2022-10-12 20:22:04 -0600

    Dashboard initial design

---
+++ frontend/src/feedback.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.css';
+
+export default function Feedback() {
+  return (
+    <div>
+      <input type="text" placeholder="Search" />
+      <ul>
+        <li>John Doe - Certificate Program Level 1 Post-Assessment Survey</li>
+        <li>Anonymous - Certificate Program Level 1 Post-Assessment Survey</li>
+        <li>Joe Brown - Certificate Program Level 1 Pre-Assessment Survey</li>
+      </ul>
+    </div>
+  );
+}
