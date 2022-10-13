d69f0b84e3bf579b475880e06bfc810c66c505d0
Jinglong <jinglong@ualberta.ca>
2022-10-13 16:06:16 -0600

    save

--- storyTellingBackend/questionnaire/models.py
+++ storyTellingBackend/questionnaire/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 import uuid
+from user.models import User
 
 # Create your models here.
 class Answer(models.Model):
     id = models.UUIDField(primary_key = True , auto_created = True , default = uuid.uuid4)
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000)
-    url = models.CharField(max_length=200,default='',blank=True,null=True)
-    
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.CharField(max_length=200,default='',blank=True,null=True)    
+    author = models.ForeignKey(User, on_delete=models.CASCADE,default='unknown')
 
 class Question(models.Model):
     id = models.UUIDField(primary_key = True , auto_created = True , default = uuid.uuid4)
