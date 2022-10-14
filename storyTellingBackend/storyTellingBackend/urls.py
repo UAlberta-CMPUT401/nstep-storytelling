a5d6525aab382c387175093af4808a3b09cb07f2
Asad <asadidrees70@gmail.com>
2022-10-13 19:51:43 -0600

    added user endpoint

--- storyTellingBackend/storyTellingBackend/urls.py
+++ storyTellingBackend/storyTellingBackend/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path,re_path
+from user import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users',views.UserList.as_view(),name='UserList'),
+    re_path(r'api/users/(?P<pk>[(-z)]{36})/', views.UserDetail.as_view(),name='SingleUser'),
+
 ]
--- storyTellingBackend/user/views.py
+++ storyTellingBackend/user/views.py
@@ -1,3 +1,76 @@
 from django.shortcuts import render
 
+from user.models import *
+from user.serializers import *
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 # Create your views here.
+
+class UserList(APIView):
+    """
+    List all users, 
+    """
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserInListSerializer(users, many=True)
+        response = {'total': len(serializer.data),
+                    'users': serializer.data,
+                    }
+
+        return Response(response)
+    
+    
+class UserDetail(APIView):
+    """
+    Retrieve, update or delete a User instance.
+    """
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                        "status": 0,
+                        "message": "User updated"
+                        }
+            return Response(response)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        user = self.get_object(pk)
+
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                        "status": 0,
+                        "message": "User modified"
+                        }
+            return Response(response)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user.delete()
+        response = {
+            "status": 0,
+            "message": "User deleted"
+            }
+        return Response(response)
