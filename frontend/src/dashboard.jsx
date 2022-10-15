ee69bdfa03f7a87730a4d0ae692c829a92083724
Théo <68571255+TheosTechGH@users.noreply.github.com>
2022-10-15 17:45:53 -0600

    Fix path to navbar

--- frontend/src/dashboard.jsx
+++ frontend/src/dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Tabs, Tab } from "@mui/material";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
 import Forms from "./forms";
 import Feedback from "./feedback";
 import "./styles/App.css";
