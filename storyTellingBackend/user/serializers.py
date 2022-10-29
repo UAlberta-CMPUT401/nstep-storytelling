d800347c89e3e854a05cb67fa25c04b9217cd902
Jinglong <jinglong@ualberta.ca>
2022-10-29 11:15:42 -0600

    fix user app

--- storyTellingBackend/user/serializers.py
+++ storyTellingBackend/user/serializers.py
@@ -2,14 +2,14 @@ from django.db.models import fields
 from rest_framework import serializers
 from user.models import *
 
+class AddingUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username','profileImage',"email"]
+
+
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        fields = ['username','profileImage']
-
-
-class UserInListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username','profileImage']
+        fields = "__all__"
