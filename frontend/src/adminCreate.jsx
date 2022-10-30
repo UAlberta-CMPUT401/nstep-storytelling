4747b17f03f8957080fc681be789a4d35431714d
Jason Hoang <jhoang2@ualberta.ca>
2022-10-30 00:14:00 -0600

    Finish questionnaire creating and saving functionality

--- frontend/src/adminCreate.jsx
+++ frontend/src/adminCreate.jsx
@@ -5,13 +5,16 @@ import Button from '@mui/material/Button';
 import ElementSelector from "./components/ElementSelector";
 import Navbar from "./components/Navbar";
 import TextInput from "./components/TextInput";
-import { createQuestion, deleteQuestion, createQuestionnaire } from './service';
+import {
+  createQuestion, deleteQuestion, createQuestionnaire, patchQuestionnaire, patchQuestion,
+} from './service';
 import "./styles/adminCreate.css";
 
 export default function AdminCreate() {
   // const [isToggled, setIsToggled] = React.useState(false);
   const [questionList, setQuestionList] = React.useState([]);
   const [qid, setQid] = React.useState("hi");
+  const [formTitle, setFormTitle] = React.useState("");
 
   React.useEffect(async () => {
     const res = await createQuestionnaire("Form title", "Form description");
@@ -19,6 +22,11 @@ export default function AdminCreate() {
     setQid(res.id);
     console.log(qid);
   }, []);
+
+  const handleTitle = (e) => {
+    setFormTitle(e.target.value);
+    console.log(formTitle);
+  };
 
   const handleChange = (e) => {
     const newQuestionList = [...questionList];
@@ -32,6 +40,17 @@ export default function AdminCreate() {
     });
     setQuestionList(newQuestionList);
     console.log(questionList);
+  };
+
+  const handleSave = async (e) => {
+    const res = await patchQuestionnaire(qid, formTitle);
+    console.log(res);
+
+    const newQuestionList = [...questionList];
+    await Promise.all(newQuestionList.map(async (question) => {
+      const q = patchQuestion(qid, question.id, question.content);
+      console.log(q);
+    }));
   };
 
   const addQuestion = async () => {
@@ -65,14 +84,14 @@ export default function AdminCreate() {
       <Navbar />
       <div className="admin-create-body">
         <div style={{ textAlign: "center" }}>
-          <TextField id="filled-basic" label="Form title" variant="filled" />
+          <TextField id="filled-basic" label="Form title" variant="filled" onChange={handleTitle} />
         </div>
         <div>
           {questionList.map((question) => (
             <TextInput
               key={question.id}
               id={question.id}
-              value={question.name}
+              value={question.content}
               onChange={handleChange}
               onClick={removeQuestion}
             />
@@ -89,7 +108,7 @@ export default function AdminCreate() {
         </div>
         <div style={{ textAlign: "center" }} className="save-and-return">
           <a href="/dashboard" className="admin-save-button">
-            <Button variant="contained">Save & Return</Button>
+            <Button variant="contained" onClick={handleSave}>Save & Return</Button>
           </a>
         </div>
       </div>
--- frontend/src/service.js
+++ frontend/src/service.js
@@ -13,6 +13,12 @@ const deleteQuestion = async (questionnaireId, questionId) => {
   const res = await axios.delete(`${api}/questionnaire/${questionnaireId}/question/${questionId}/`);
   return res.data;
 };
+const patchQuestion = async (questionnaireId, questionId, content) => {
+  const question = await axios.patch(`${api}/questionnaire/${questionnaireId}/question/${questionId}/`, {
+    content,
+  });
+  return question.data;
+};
 const createQuestionnaire = async (title, description) => {
   const questionnaire = await axios.post(`${api}/questionnaire/`, {
     title,
@@ -20,7 +26,13 @@ const createQuestionnaire = async (title, description) => {
   });
   return questionnaire.data;
 };
+const patchQuestionnaire = async (questionnaireId, title) => {
+  const questionnaire = await axios.patch(`${api}/questionnaire/${questionnaireId}/`, {
+    title,
+  });
+  return questionnaire.data;
+};
 
 export {
-  createQuestion, deleteQuestion, createQuestionnaire,
+  createQuestion, deleteQuestion, createQuestionnaire, patchQuestionnaire, patchQuestion,
 };
