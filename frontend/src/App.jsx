590b7dc8c4fdcdf8568c4554227ff66b3066656d
Théo <68571255+TheosTechGH@users.noreply.github.com>
2022-11-01 16:57:50 -0600

    Refactoring

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import Welcome from "./welcome";
 import Login from "./login";
 import Dashboard from "./dashboard";
-import AdminCreate from "./createQuestionnaire";
-import UserCreate from "./answerQuestionnaire";
+import CreateQuestionnaire from "./createQuestionnaire";
+import AnswerQuestionnaire from "./answerQuestionnaire";
 import Submitted from "./submitted";
 import "./styles/App.css";
 
@@ -15,8 +15,8 @@ export default function App() {
       <Route path="/" element={<Welcome />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/create" element={<AdminCreate />} />
-      <Route path="/newForm" element={<UserCreate />} />
+      <Route path="/create" element={<CreateQuestionnaire />} />
+      <Route path="/newForm" element={<AnswerQuestionnaire />} />
       <Route path="/submitted" element={<Submitted />} />
       <Route path="/createAdmin" element={<Submitted />} />
     </Routes>
