58d1f5a07576156b7c2fe77ab35856ffdd260723
Mohammed Azad <mmazad@ualberta.ca>
2022-11-12 16:28:01 -0700

    Rollback
    rolling back to old code

--- frontend/src/Webcam.jsx
+++ frontend/src/Webcam.jsx
@@ -3,14 +3,29 @@
 import React, {
   useRef, useEffect, useState,
 } from 'react';
-import { ReactMediaRecorder, useReactMediaRecorder } from "react-media-recorder";
+import { useReactMediaRecorder } from "react-media-recorder";
 
-export default function WebCamTest() {
+// const [hasCameraPermission, setHasCameraPermission] = useState();
+// const [hasMicrohonePermission, setHasMicrophonePermission] = useState();
+
+// useEffect(() => {
+//   (async () => {
+//     const cameraPermission = await Camera.requestCameraPermissionsAsync();
+//     const microphonePermission = await Camera.requestMicrophonePermissionsAsync();
+//     const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+
+//     setHasCameraPermission(cameraPermission.status === "granted");
+//     setHasMicrophonePermission(microphonePermission.status === "granted");
+//     setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+//   })();
+// }, []);
+
+function WebCamTest() {
+  // const {
+  //   status, startRecording, stopRecording, mediaBlobUrl,
+  // } = useReactMediaRecorder({ video: true, audio: true });
   const videoRef = useRef(null);
   const mediaRecorder = useRef(null);
-  const {
-    status, startRecording, stopRecording, mediaBlobUrl,
-  } = useReactMediaRecorder({ video: true });
 
   const getVideo = () => {
     navigator.mediaDevices
@@ -30,12 +45,16 @@ export default function WebCamTest() {
   return (
     <div>
       <video ref={videoRef} />
-      <p>{status}</p>
-      <button onClick={startRecording}>Start Recording</button>
-      <button onClick={stopRecording}>Stop Recording</button>
-      <video src={mediaBlobUrl} controls autoPlay loop />
     </div>
+
+  // <div>
+  //   hello world
+  //   <p>{status}</p>
+  //   <button onClick={startRecording}>Start Recording</button>
+  //   <button onClick={stopRecording}>Stop Recording</button>
+  //   <video src={mediaBlobUrl} controls autoPlay loop />
+  // </div>
   );
 }
 
-// export default WebCamTest;
+export default WebCamTest;
