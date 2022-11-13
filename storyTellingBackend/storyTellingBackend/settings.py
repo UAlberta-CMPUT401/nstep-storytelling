1e0d241057545ff35b9a06fd74b88e03e2ff6c9f
Jinglong <jinglong@ualberta.ca>
2022-11-12 21:06:11 -0700

    fix bug, change url

--- storyTellingBackend/storyTellingBackend/settings.py
+++ storyTellingBackend/storyTellingBackend/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nstep_database',
         'USER': 'root',
-        'PASSWORD': 'mypassword',
+        'PASSWORD': 'Dragon123!',
         'HOST': 'localhost',
         'PORT': '',
     }
