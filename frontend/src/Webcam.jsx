527b902c095eebf8b463ac9e24e6089e9d34a32b
PileUp120 <60121192+PileUp120@users.noreply.github.com>
2022-11-14 09:36:39 -0700

    somehow working now

--- frontend/src/Webcam.jsx
+++ frontend/src/Webcam.jsx
@@ -21,39 +21,39 @@ import { useReactMediaRecorder } from "react-media-recorder";
 // }, []);
 
 function WebCamTest() {
-  // const {
-  //   status, startRecording, stopRecording, mediaBlobUrl,
-  // } = useReactMediaRecorder({ video: true, audio: true });
-  const videoRef = useRef(null);
-  const mediaRecorder = useRef(null);
+  const {
+    status, startRecording, stopRecording, mediaBlobUrl,
+  } = useReactMediaRecorder({ video: true, audio: true });
+  // const videoRef = useRef(null);
+  // const mediaRecorder = useRef(null);
 
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true, video: { width: 640, height: 360 } })
-      .then((stream) => {
-        const video = videoRef.current; video.srcObject = stream; video.play();
-        mediaRecorder.current = new MediaRecorder(stream);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
+  // const getVideo = () => {
+  //   navigator.mediaDevices
+  //     .getUserMedia({ audio: true, video: { width: 640, height: 360 } })
+  //     .then((stream) => {
+  //       const video = videoRef.current; video.srcObject = stream; video.play();
+  //       mediaRecorder.current = new MediaRecorder(stream);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+  // useEffect(() => {
+  //   getVideo();
+  // }, [videoRef]);
 
   return (
-    <div>
-      <video ref={videoRef} />
-    </div>
-
   // <div>
-  //   hello world
-  //   <p>{status}</p>
-  //   <button onClick={startRecording}>Start Recording</button>
-  //   <button onClick={stopRecording}>Stop Recording</button>
-  //   <video src={mediaBlobUrl} controls autoPlay loop />
+  //   <video ref={videoRef} />
   // </div>
+
+    <div>
+      hello world
+      <p>{status}</p>
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      <video src={mediaBlobUrl} controls autoPlay loop />
+    </div>
   );
 }
 
