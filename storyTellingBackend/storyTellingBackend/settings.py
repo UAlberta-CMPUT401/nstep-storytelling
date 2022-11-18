196d05a1318553c8aac6c29c73cdd48bd8b67476
Jinglong <jinglong@ualberta.ca>
2022-11-17 20:22:47 -0700

    fix bug signup

--- storyTellingBackend/storyTellingBackend/settings.py
+++ storyTellingBackend/storyTellingBackend/settings.py
@@ -133,6 +133,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = (
+        'django.contrib.auth.backends.ModelBackend',
+    )
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
