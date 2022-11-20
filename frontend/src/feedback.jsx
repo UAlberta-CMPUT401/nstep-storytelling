587ca38c0f2bc0d4e76bafdfdfddba7232359235
Jinglong <jinglong@ualberta.ca>
2022-11-19 17:43:47 -0700

    finish feedbacks showing

--- frontend/src/feedback.jsx
+++ frontend/src/feedback.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import './styles/Feedback.css';
@@ -7,9 +8,9 @@ import Stack from '@mui/material/Stack';
 import Card from '@mui/material/Card';
 import IconButton from '@mui/material/IconButton';
 import Grid from '@mui/material/Grid';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
+import Link from '@mui/material/Link';
+import ListItem from '@mui/material/ListItem';
+import Autocomplete from '@mui/material/Autocomplete';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
@@ -20,15 +21,16 @@ import MoreIcon from '@mui/icons-material/MoreVert';
 import Paper from '@mui/material/Paper';
 import { getAllFeedback } from './service';
 
-class oneFeedback extends React.Component {
+class OneFeedback extends React.Component {
   constructor(props) {
     super(props);
+    console.log("props");
     console.log(props);
-    // this.state = {
-    //   id: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    //   title: "Program 1",
-    //   time: "2022-11-19T13:48:42.051853-07:00"
-    // }
+    this.state = {
+      id: this.props.data.id,
+      title: this.props.data.questionnaire.title,
+      time: this.props.data.time,
+    };
     // this.getInfo();
   }
 
@@ -45,21 +47,19 @@ class oneFeedback extends React.Component {
 
   render() {
     return (
-      <Card variant="outlined">
-        <Stack direction="row" spacing={2}>
-          <li>
-            {this.props.id}
-            {' '}
-            -
-            {' '}
-            {this.props.title}
-            {' '}
-            -
-            {' '}
-            {this.props.time}
-          </li>
-        </Stack>
-      </Card>
+      <Link href={`/feedback/${this.state.id}/`}>
+        <li>
+          {this.state.id}
+          {' '}
+          -
+          {' '}
+          {this.state.title}
+          {' '}
+          -
+          {' '}
+          {this.state.time}
+        </li>
+      </Link>
     );
   }
 }
@@ -73,15 +73,18 @@ class FeedbackList extends React.Component {
   renderList() {
     try {
       const { feedbacklist } = this.props;
-      console.log(feedbacklist);
       if (feedbacklist === undefined) {
         return null;
       }
-
-      return feedbacklist.length === 0
+      const myList = Object.values(feedbacklist);
+      console.log("myList");
+      console.log(myList);
+      return myList.length === 0
         ? "NoFeedbacks"
-        : (feedbacklist?.map((item) => (
-          <oneFeedback id={item.id} />
+        : (myList?.map((item) => (
+          <ListItem key={item.id}>
+            <OneFeedback data={item} />
+          </ListItem>
         )));
     } catch (e) {
       console.log(e);
@@ -106,62 +109,58 @@ class FeedbackList extends React.Component {
   }
 }
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
-
 export default class Feedback extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
+      current: [],
+      keys: [],
+      title: "1",
     };
+    this.handleForm = this.handleForm.bind(this);
+    this.search = this.search.bind(this);
   }
 
   async componentDidMount() {
     const response = await getAllFeedback();
     const myData = response.results;
-    this.setState(myData);
-    console.log(myData);
+    this.setState({ data: myData });
+    this.setState({ current: myData });
+    const myKeys = [];
+    myData.forEach((item) => {
+      console.log("item.questionnaire.title");
+      console.log(item.questionnaire.title);
+      myKeys.push({ title: item.questionnaire.title });
+    });
+    this.setState({ keys: myKeys });
+    console.log("this.state");
+    console.log(this.state);
+  }
+
+  handleForm(e) {
+    console.log(e);
+    console.log(e.target.value);
+    this.setState({
+      title: e.target.value,
+    });
+    this.search(this.state.title);
+  }
+
+  search(keyword) {
+    const newCurrent = [];
+    this.state.data.forEach((item) => {
+      const myText = item.questionnaire.title;
+      if (myText.includes(keyword)) {
+        newCurrent.push(item);
+      }
+    });
+    this.setState({ current: newCurrent });
   }
 
   render() {
+    const { title } = this.state;
+
     return (
       <Paper
         sx={{
@@ -180,22 +179,32 @@ export default class Feedback extends React.Component {
         variant="outlined"
         component="form"
       >
-        <div className="feedback">
-          <InputBase
-            align="center"
-            id="filled-search"
-            label="Search"
-            type="search"
-            variant="filled"
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="Search for stories"
-            inputProps={{ 'aria-label': 'Search for stories' }}
+        <Grid className="feedback">
+          <Autocomplete
+            freeSolo
+            id="free-solo-2-demo"
+            disableClearable
+            options={this.state.keys}
+            getOptionLabel={(option) => option.title || ""}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="title"
+                InputProps={{
+                  ...params.InputProps,
+                  type: 'search',
+                }}
+                name="title"
+                value={title}
+                onChange={this.handleForm}
+              />
+            )}
           />
-          <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+          <IconButton type="button" sx={{ p: '10px', display: "inline-block" }} aria-label="search">
             <SearchIcon />
           </IconButton>
-        </div>
-        <FeedbackList feedbacklist={this.state.data} />
+        </Grid>
+        <FeedbackList feedbacklist={this.state.current} />
       </Paper>
     );
   }
