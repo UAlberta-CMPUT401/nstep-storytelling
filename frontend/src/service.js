f2ba55c89d69d15d8489c394f3f60e1772a55062
Jason Hoang <jhoang2@ualberta.ca>
2022-11-22 22:40:55 -0700

    Fix getting token bug

--- frontend/src/service.js
+++ frontend/src/service.js
@@ -1,33 +1,39 @@
 /* eslint-disable camelcase */
-import { accordionDetailsClasses } from '@mui/material';
 import axios from 'axios';
 import 'regenerator-runtime';
 
 const api = 'http://localhost:8000/api';
-const token = localStorage.getItem('jwtToken');
-const id = localStorage.getItem('userID');
-
-const header = {
-  headers: {
-    Authorization: `Token ${token}`,
-  },
-};
 
 const createQuestion = async (questionnaireId, content) => {
+  const token = localStorage.getItem('jwtToken');
   const question = await axios.post(`${api}/questionnaire/${questionnaireId}/question/`, {
     content,
-  }, header);
+  }, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return question.data;
 };
 const deleteQuestion = async (questionnaireId, questionId) => {
-  const res = await axios.delete(`${api}/questionnaire/${questionnaireId}/question/${questionId}/`, header);
+  const token = localStorage.getItem('jwtToken');
+  const res = await axios.delete(`${api}/questionnaire/${questionnaireId}/question/${questionId}/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return res.data;
 };
 const patchQuestion = async (questionnaireId, questionId, content, allowRecording) => {
+  const token = localStorage.getItem('jwtToken');
   const question = await axios.patch(`${api}/questionnaire/${questionnaireId}/question/${questionId}/`, {
     content: content,
     allow_recording: allowRecording,
-  }, header);
+  }, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return question.data;
 };
 const getQuestion = async (questionnaireId, questionId) => {
@@ -35,16 +41,26 @@ const getQuestion = async (questionnaireId, questionId) => {
   return question.data;
 };
 const createQuestionnaire = async (title, description) => {
+  const token = localStorage.getItem('jwtToken');
   const questionnaire = await axios.post(`${api}/questionnaire/`, {
     title,
     description,
-  }, header);
+  }, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return questionnaire.data;
 };
 const patchQuestionnaire = async (questionnaireId, title) => {
+  const token = localStorage.getItem('jwtToken');
   const questionnaire = await axios.patch(`${api}/questionnaire/${questionnaireId}/`, {
     title,
-  }, header);
+  }, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return questionnaire.data;
 };
 const getQuestionnaires = async () => {
@@ -52,7 +68,12 @@ const getQuestionnaires = async () => {
   return questionnaires.data;
 };
 const deleteQuestionnaire = async (questionnaireId) => {
-  const res = await axios.delete(`${api}/questionnaire/${questionnaireId}/`, header);
+  const token = localStorage.getItem('jwtToken');
+  const res = await axios.delete(`${api}/questionnaire/${questionnaireId}/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return res.data;
 };
 const getQuestionnaire = async (questionnaireId) => {
@@ -64,41 +85,77 @@ const createFeedback = async (questionnaireId, answers) => {
   return feedback.data;
 };
 const deleteFeedback = async (feedbackId) => {
-  const res = await axios.delete(`${api}/feedback/${feedbackId}/`, header);
+  const token = localStorage.getItem('jwtToken');
+  const res = await axios.delete(`${api}/feedback/${feedbackId}/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return res.data;
 };
 
 const getFeedback = async (feedbackId) => {
-  const feedback = await axios.get(`${api}/feedback/${feedbackId}/`, header);
+  const token = localStorage.getItem('jwtToken');
+  const feedback = await axios.get(`${api}/feedback/${feedbackId}/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return feedback.data;
 };
 const createUser = async (username, password, email, is_superuser, user_permissions) => {
+  const token = localStorage.getItem('jwtToken');
   const user = await axios.post(`${api}/user/`, {
     username,
     password,
     email,
     is_superuser,
     user_permissions,
-  }, header);
+  }, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return user.data;
 };
 const getUsers = async () => {
-  const users = await axios.get(`${api}/user/`, header);
+  const token = localStorage.getItem('jwtToken');
+  const users = await axios.get(`${api}/user/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return users.data;
 };
 
 const getUser = async (userId) => {
-  const user = await axios.get(`${api}/user/${userId}/`, header);
+  const token = localStorage.getItem('jwtToken');
+  console.log(token);
+  const user = await axios.get(`${api}/user/${userId}/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return user.data;
 };
 
 const deleteUser = async (userId) => {
-  const res = await axios.delete(`${api}/user/${userId}/`, header);
+  const token = localStorage.getItem('jwtToken');
+  const res = await axios.delete(`${api}/user/${userId}/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return res.data;
 };
 
 const getAllFeedback = async () => {
-  const feedback = await axios.get(`${api}/feedback/`, header);
+  const token = localStorage.getItem('jwtToken');
+  const feedback = await axios.get(`${api}/feedback/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
   return feedback.data;
 };
 
