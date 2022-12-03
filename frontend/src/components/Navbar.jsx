0e3a83fddc887f301f0a8dea5e18542900dd6dbf
Jason Hoang <jhoang2@ualberta.ca>
2022-12-03 14:00:08 -0700

    Fix navbar link and push new build

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import logo from '../../assets/NSTEP_Horizontal-Logo_Color_WithTagline.png';
 
 const Navbar = () => (
   <div className="navbar">
     <a id="logo-link" href="https://nstep.ca/"><img src={logo} aria-label="NSTEP logo" className="navbar-logo" /></a>
-    <a href="http://localhost:8080/" className="navbar-name">Tell your story!</a>
+    <Link to="/" className="navbar-name">Tell your story!</Link>
   </div>
 );
 
